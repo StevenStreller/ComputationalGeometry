fd28d2e2e00904075b85c3d1edac65d93a90def4
Steven Streller <jon-steven.streller@bgst.dlrg.de>
2024-04-23 20:35:01 +0200

    Half-Edge

--- CgSceneGraph/CgHalfEdgeTriangleMesh.cpp
+++ CgSceneGraph/CgHalfEdgeTriangleMesh.cpp
@@ -2,6 +2,7 @@
 
 #include "CgBase/CgEnums.h"
 #include "CgUtils/ObjLoader.h"
+#include <iostream>
 
 
 CgHalfEdgeTriangleMesh::CgHalfEdgeTriangleMesh():
@@ -104,6 +105,70 @@ const std::vector<CgBaseHeFace*>& CgHalfEdgeTriangleMesh::getFaces() const
     return m_faces;
 }
 
+void parseOBJ(const std::vector<glm::vec3>& vertices, const std::vector<glm::vec3>& normals, const std::vector<unsigned int>& indices) {
+    std::vector<CgHeVert*> cgHeVerts(vertices.size());
+    std::vector<CgHeEdge*> cgHeEdges;
+    std::vector<CgHeFace*> cgHeFaces;
+
+    for(unsigned int i = 0; i < vertices.size(); i++) {
+        cgHeVerts[i] = new CgHeVert();
+        cgHeVerts[i]->m_position = vertices[i];
+    }
+
+    std::cout << indices.size() << std::endl;
+    // Create half-edges for each face
+    for (size_t i = 0; i < indices.size(); i += 3) {
+        unsigned int vIndex1 = indices[i];
+        unsigned int vIndex2 = indices[i + 1];
+        unsigned int vIndex3 = indices[i + 2];
+
+        // Create edges
+        CgHeEdge* edge1 = new CgHeEdge();
+        CgHeEdge* edge2 = new CgHeEdge();
+        CgHeEdge* edge3 = new CgHeEdge();
+
+        cgHeEdges.push_back(edge1);
+        cgHeEdges.push_back(edge2);
+        cgHeEdges.push_back(edge3);
+
+        // TODO
+        // Assign edge to vertex
+        cgHeVerts[vIndex1]->m_edge = edge3;
+        cgHeVerts[vIndex2]->m_edge = edge1;
+        cgHeVerts[vIndex3]->m_edge = edge2;
+
+        // Assign vertices to edges
+        cgHeEdges[cgHeEdges.size() - 3]->m_vert = cgHeVerts[vIndex1];
+        cgHeEdges[cgHeEdges.size() - 2]->m_vert = cgHeVerts[vIndex2];
+        cgHeEdges[cgHeEdges.size() - 1]->m_vert = cgHeVerts[vIndex3];
+
+
+        // Assign next pointers
+        cgHeEdges[cgHeEdges.size() - 3]->m_next = cgHeEdges[cgHeEdges.size() - 2];
+        cgHeEdges[cgHeEdges.size() - 2]->m_next = cgHeEdges[cgHeEdges.size() - 1];
+        cgHeEdges[cgHeEdges.size() - 1]->m_next = cgHeEdges[cgHeEdges.size() - 3];
+
+        // Assign face to edge
+        CgHeFace* face = new CgHeFace();
+        cgHeEdges[cgHeEdges.size() - 3]->m_face = face;
+        cgHeEdges[cgHeEdges.size() - 2]->m_face = face;
+        cgHeEdges[cgHeEdges.size() - 1]->m_face = face;
+        cgHeFaces.push_back(face);
+
+        // Assign edge to face
+        face->m_edge = cgHeEdges[cgHeEdges.size() - 3];
+    }
+
+
+    // test
+    int index = 1;
+    CgHeEdge* edge = cgHeFaces[index]->m_edge;
+    do {
+        edge = edge->m_next;
+        std::cout << "index: " << index << ", " << "x: " << edge->m_vert->m_position.x << std::endl;
+    } while (edge != cgHeFaces[index]->m_edge);
+}
+
 void CgHalfEdgeTriangleMesh::init( std::string filename)
 {
     std::vector<glm::vec3> temp_vertices;
@@ -118,6 +183,7 @@ void CgHalfEdgeTriangleMesh::init( std::string filename)
     loader.getNormalData(temp_vertnormals);
     loader.getFaceIndexData(temp_indices);
 
+    parseOBJ(temp_vertices, temp_vertnormals, temp_indices);
 
     // now need to convert into HalfEdge Datastructure :-) ...
 }
