9cf8485a50da8e23ebd108361119ceac37e13f01
Steven Streller <jon-steven.streller@bgst.dlrg.de>
2024-05-24 14:45:45 +0200

    Maybe kdtree implementiert

--- CgSceneGraph/CgPointCloud.cpp
+++ CgSceneGraph/CgPointCloud.cpp
@@ -3,30 +3,30 @@
 #include "CgUtils/ObjLoader.h"
 #include <glm/gtc/matrix_transform.hpp>
 #include <algorithm>
+#include <iostream>
 
+CgPointCloud::Node::Node(const glm::vec3 &point)
+        : location(point), leftChild(nullptr), rightChild(nullptr) {}
 
-CgPointCloud::CgPointCloud():
-CgPointCloud::CgPointCloud(51)
-{
+CgPointCloud::CgPointCloud() :
+        CgPointCloud::CgPointCloud(51) {
 
 }
 
-CgPointCloud::CgPointCloud(int id):
-m_type(Cg::PointCloud),
-m_id(id)
-{
+CgPointCloud::CgPointCloud(int id) :
+        m_type(Cg::PointCloud),
+        m_id(id) {
 
-    m_vertices.push_back(glm::vec3(0.0,0.0,0.0));
-    m_vertex_normals.push_back(glm::vec3(0.0,0.0,1.0));
-    m_vertex_colors.push_back(glm::vec3(0.0,0.0,1.0));
+    m_vertices.push_back(glm::vec3(0.0, 0.0, 0.0));
+    m_vertex_normals.push_back(glm::vec3(0.0, 0.0, 1.0));
+    m_vertex_colors.push_back(glm::vec3(0.0, 0.0, 1.0));
 
     calculateSplatOrientations();
 
 }
 
 
-CgPointCloud::~CgPointCloud()
-{
+CgPointCloud::~CgPointCloud() {
     m_vertices.clear();
     m_vertex_normals.clear();
     m_vertex_colors.clear();
@@ -34,48 +34,53 @@ CgPointCloud::~CgPointCloud()
 }
 
 
-void CgPointCloud::calculateSplatOrientations()
-{
-  // calculate local coordinate system for splats (arbitrary orientation of ellipse in plane)
-  // replace this if you have the real coordinate system, use up vector = y-Axis of your local coordinate system instead of getPerpendicularVector(...)
+void CgPointCloud::calculateSplatOrientations() {
+    // calculate local coordinate system for splats (arbitrary orientation of ellipse in plane)
+    // replace this if you have the real coordinate system, use up vector = y-Axis of your local coordinate system instead of getPerpendicularVector(...)
 
-  m_splat_orientations.clear();
-  m_splat_scaling.clear();
-  m_splat_indices.clear();
+    m_splat_orientations.clear();
+    m_splat_scaling.clear();
+    m_splat_indices.clear();
 
-  for(unsigned int i=0;i<m_vertices.size();i++)
-    {
-      glm::mat4 lookAt_matrix(glm::lookAt(glm::vec3(m_vertices[i]),glm::vec3(m_vertices[i]-m_vertex_normals[i]),getPerpendicularVector(m_vertex_normals[i])));
-      m_splat_orientations.push_back(lookAt_matrix);
-      m_splat_scaling.push_back(glm::vec2(0.02,0.005));
+    for (unsigned int i = 0; i < m_vertices.size(); i++) {
+        glm::mat4 lookAt_matrix(glm::lookAt(glm::vec3(m_vertices[i]), glm::vec3(m_vertices[i] - m_vertex_normals[i]),
+                                            getPerpendicularVector(m_vertex_normals[i])));
+        m_splat_orientations.push_back(lookAt_matrix);
+        m_splat_scaling.push_back(glm::vec2(0.02, 0.005));
 
-      // use all points for splatting by default
-      m_splat_indices.push_back(i);
+        // use all points for splatting by default
+        m_splat_indices.push_back(i);
     }
 
 
 }
 
 
-void CgPointCloud::init( std::string filename, bool cheat_normals)
-{
+void CgPointCloud::init(std::string filename, bool cheat_normals) {
     m_vertices.clear();
     m_vertex_normals.clear();
     m_vertex_colors.clear();
     m_splat_orientations.clear();
     m_splat_scaling.clear();
     m_splat_indices.clear();
-
     // load obj File
     ObjLoader loader;
     loader.load(filename);
     loader.getPositionData(m_vertices);
 
+    CgPointCloud::Node *root = kdtree(m_vertices);
+    if (root) {
+        std::cout << "Root node location: (" << root->location.x << ", " << root->location.y << ", " << root->location.z
+                  << ")\n";
+    } else {
+        std::cout << "The KD-tree is empty.\n";
+    }
+
 
     // do this for cheating with the normals
     // you need to replace this by a normal estimation algorithm
-    if(cheat_normals)
-      loader.getNormalData(m_vertex_normals);
+    if (cheat_normals)
+        loader.getNormalData(m_vertex_normals);
 
 
     // calculate local coordinate system for splats (arbitrary orientation of ellipse in plane)
@@ -84,84 +89,104 @@ void CgPointCloud::init( std::string filename, bool cheat_normals)
     calculateSplatOrientations();
 
     //add a standard color for each point if lighting turned off
-    for(unsigned int i=0;i<m_vertices.size();i++)
-      {
-          m_vertex_colors.push_back(glm::vec3(0.0,1.0,0.0));
-      }
+    for (unsigned int i = 0; i < m_vertices.size(); i++) {
+        m_vertex_colors.push_back(glm::vec3(0.0, 1.0, 0.0));
+    }
 
 
     //test of getNeartestNeighbors(..) method
     // generates blue dots on the tail of the bunny
 
-    unsigned int k=50;
-    std::vector<int> neighbors = getNearestNeighbors(0,k);
+    unsigned int k = 3;
+    std::vector<int> neighbors = getNearestNeighbors(0, k);
 
-    for(unsigned int i=0;i<k;i++)
-      {
-        m_vertex_colors[neighbors[i]]=glm::vec3(0.0,0.0,1.0);
-      }
+    for (unsigned int i = 0; i < k; i++) {
+        m_vertex_colors[neighbors[i]] = glm::vec3(0.0, 0.0, 1.0);
+    }
 }
 
 
-std::vector<int> CgPointCloud::getNearestNeighbors(int current_point,unsigned int k)
-{
+std::vector<int> CgPointCloud::getNearestNeighbors(int current_point, unsigned int k) {
 
-  glm::vec3 q= m_vertices[current_point];
+    glm::vec3 q = m_vertices[current_point];
 
-  std::vector<std::pair<double,int>> distances;
+    std::vector<std::pair<double, int>> distances;
 
-  // very inefficient, just to show that it works for rendering colored neighborhood
-  // use min heap for real purposes
+    // very inefficient, just to show that it works for rendering colored neighborhood
+    // use min heap for real purposes
 
 
-  for(unsigned int i=0;i<m_vertices.size();i++)
-    {
-      double dist=glm::distance(m_vertices[i],q);
+    for (unsigned int i = 0; i < m_vertices.size(); i++) {
+        double dist = glm::distance(m_vertices[i], q);
 
-      distances.push_back(std::make_pair(dist,i));
+        distances.push_back(std::make_pair(dist, i));
     }
 
-    std::sort(distances.begin(),distances.end());
+    std::sort(distances.begin(), distances.end());
 
     std::vector<int> erg;
 
-   for(unsigned int i=0;i<k;i++)
-    {
-       erg.push_back(distances[i].second);
-     }
+    for (unsigned int i = 0; i < k; i++) {
+        erg.push_back(distances[i].second);
+    }
 
     return erg;
-  }
+}
 
 
 // calculates an arbitrary verctor perpendicular to the given one
-glm::vec3 CgPointCloud::getPerpendicularVector(glm::vec3 arg)
-{
-  if((arg[0]==0.0)&&(arg[1]==0.0))
-    {
-    if(arg[2]==0.0)
-      return glm::vec3(0.);
+glm::vec3 CgPointCloud::getPerpendicularVector(glm::vec3 arg) {
+    if ((arg[0] == 0.0) && (arg[1] == 0.0)) {
+        if (arg[2] == 0.0)
+            return glm::vec3(0.);
 
-    return glm::vec3(0.0,1.0,0.0);
+        return glm::vec3(0.0, 1.0, 0.0);
     }
-  return glm::normalize(glm::vec3(-arg[1],arg[0],0.0));
+    return glm::normalize(glm::vec3(-arg[1], arg[0], 0.0));
 }
 
+// Implementierung der kdtree Methode
+CgPointCloud::Node* CgPointCloud::kdtree(std::vector<glm::vec3>& pointList) {
+    return buildKdTree(pointList.begin(), pointList.end(), 0);
+}
 
-
-
-const glm::vec3 CgPointCloud::getCenter() const
-{
-  glm::vec3 center(0.);
-  for(unsigned int i=0;i<m_vertices.size();i++)
-    {
-      center+=m_vertices[i];
+// Implementierung der buildKdTree Methode
+CgPointCloud::Node* CgPointCloud::buildKdTree(std::vector<glm::vec3>::iterator begin, std::vector<glm::vec3>::iterator end, int depth) {
+    if (begin == end) {
+        return nullptr;
     }
-  center/=(double)m_vertices.size();
-  return center;
+
+    int k = 3;  // Anzahl der Dimensionen
+    int axis = depth % k;
+
+    // Verwenden Sie std::nth_element, um den Median in-place zu finden
+    auto mid = begin + (end - begin) / 2;
+    std::nth_element(begin, mid, end, [axis](const glm::vec3& a, const glm::vec3& b) {
+        if (axis == 0) return a.x < b.x;
+        else if (axis == 1) return a.y < b.y;
+        else return a.z < b.z;
+    });
+
+    Node* node = new Node(*mid);
+
+    // Erstellen Sie die linken und rechten Unterbäume rekursiv
+    node->leftChild = buildKdTree(begin, mid, depth + 1);
+    node->rightChild = buildKdTree(mid + 1, end, depth + 1);
+
+    return node;
 }
 
-const std::vector<glm::vec2>& CgPointCloud::getSplatScalings() const
-{
-  return m_splat_scaling;
+
+const glm::vec3 CgPointCloud::getCenter() const {
+    glm::vec3 center(0.);
+    for (unsigned int i = 0; i < m_vertices.size(); i++) {
+        center += m_vertices[i];
+    }
+    center /= (double) m_vertices.size();
+    return center;
 }
+
+const std::vector<glm::vec2> &CgPointCloud::getSplatScalings() const {
+    return m_splat_scaling;
+}
+
--- CgSceneGraph/CgPointCloud.h
+++ CgSceneGraph/CgPointCloud.h
@@ -6,46 +6,59 @@
 #include <string>
 #include "CgBase/CgBasePointCloud.h"
 
-class CgPointCloud : public CgBasePointCloud
-{
+class CgPointCloud : public CgBasePointCloud {
 public:
-  CgPointCloud();
-  CgPointCloud(int id);
+    CgPointCloud();
 
-  ~CgPointCloud();
+    CgPointCloud(int id);
 
-  //inherited from CgBaseRenderableObject
-  Cg::ObjectType getType() const;
-  unsigned int getID() const;
+    struct Node {
+        glm::vec3 location;
+        Node *leftChild;
+        Node *rightChild;
+
+        Node(const glm::vec3 &point);
+    };
+
+    Node* kdtree(std::vector<glm::vec3>& pointList);
+
+    ~CgPointCloud();
+
+    //inherited from CgBaseRenderableObject
+    Cg::ObjectType getType() const;
+
+    unsigned int getID() const;
 
 
-  //inherited from CgBasePointCloud
+    //inherited from CgBasePointCloud
 
-  // vertex positions in local coordinates
-  const std::vector<glm::vec3>& getVertices()     const;
+    // vertex positions in local coordinates
+    const std::vector<glm::vec3> &getVertices() const;
 
-  // normal directions in local coordinates (normalized)
-  const std::vector<glm::vec3>& getVertexNormals() const;
+    // normal directions in local coordinates (normalized)
+    const std::vector<glm::vec3> &getVertexNormals() const;
 
-  // vertex colors
-  const std::vector<glm::vec3>& getVertexColors() const;
+    // vertex colors
+    const std::vector<glm::vec3> &getVertexColors() const;
 
-  // a local "look at matrix" for normal direction and the spanning directions of the tangent plane
-  const std::vector<glm::mat4>& getSplatOrientations() const;
+    // a local "look at matrix" for normal direction and the spanning directions of the tangent plane
+    const std::vector<glm::mat4> &getSplatOrientations() const;
 
-  // for rendering ellipes use a different scaling in the two spanning directions of the tangent plane
-  const std::vector<glm::vec2>& getSplatScalings() const;
+    // for rendering ellipes use a different scaling in the two spanning directions of the tangent plane
+    const std::vector<glm::vec2> &getSplatScalings() const;
 
-  // get the indices of the splats which should be rendered
-   const std::vector<unsigned int>& getSplatIndices() const;
+    // get the indices of the splats which should be rendered
+    const std::vector<unsigned int> &getSplatIndices() const;
 
-  // read a dataset from file, can cheat the normals, i.e read the mormals from file
-  void init( std::string filename, bool cheat_normals=false);
+    // read a dataset from file, can cheat the normals, i.e read the mormals from file
+    void init(std::string filename, bool cheat_normals = false);
 
-  // the center of gravity of the object, for rendering
-  const glm::vec3 getCenter() const;
+    // the center of gravity of the object, for rendering
+    const glm::vec3 getCenter() const;
 
 private:
+
+    Node* buildKdTree(std::vector<glm::vec3>::iterator begin, std::vector<glm::vec3>::iterator end, int depth);
 
     // the following demonstration methods have to be replaced by your own calculations
 
@@ -56,7 +69,7 @@ private:
     glm::vec3 getPerpendicularVector(glm::vec3 arg);
 
     // for demonstration purposes, very inefficient
-    std::vector<int> getNearestNeighbors(int current_point,unsigned int k);
+    std::vector<int> getNearestNeighbors(int current_point, unsigned int k);
 
 
     std::vector<glm::vec3> m_vertices;
@@ -74,15 +87,23 @@ private:
     const unsigned int m_id;
 
 
+    const Node *kdTree(std::vector<glm::vec3> pointList, int depth);
+
 };
 
-inline Cg::ObjectType  CgPointCloud::getType() const {return m_type;}
-inline unsigned int CgPointCloud::getID() const {return m_id;}
-inline const std::vector<glm::vec3>& CgPointCloud::getVertices() const{return m_vertices;}
-inline const std::vector<glm::vec3>& CgPointCloud::getVertexNormals() const{return m_vertex_normals;}
-inline const std::vector<glm::vec3>& CgPointCloud::getVertexColors() const{return m_vertex_colors;}
-inline const std::vector<glm::mat4>& CgPointCloud::getSplatOrientations() const{return m_splat_orientations;}
-inline const std::vector<unsigned int>& CgPointCloud::getSplatIndices() const{return m_splat_indices;}
+inline Cg::ObjectType CgPointCloud::getType() const { return m_type; }
+
+inline unsigned int CgPointCloud::getID() const { return m_id; }
+
+inline const std::vector<glm::vec3> &CgPointCloud::getVertices() const { return m_vertices; }
+
+inline const std::vector<glm::vec3> &CgPointCloud::getVertexNormals() const { return m_vertex_normals; }
+
+inline const std::vector<glm::vec3> &CgPointCloud::getVertexColors() const { return m_vertex_colors; }
+
+inline const std::vector<glm::mat4> &CgPointCloud::getSplatOrientations() const { return m_splat_orientations; }
+
+inline const std::vector<unsigned int> &CgPointCloud::getSplatIndices() const { return m_splat_indices; }
 
 
 #endif // CGPOINTCLOUD_H
